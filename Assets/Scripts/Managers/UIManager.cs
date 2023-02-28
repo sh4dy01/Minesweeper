9f8f8b78ba5701d7b62b30de314822cefc03b5f5
Hugo.M <hmaestracci@gaming.tech>
2023-02-28 08:03:25 +0100

    namespace added

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,57 +1,59 @@
-using Managers;
 using TMPro;
 using UnityEngine;
 
-public class UIManager : MonoBehaviour
+namespace Managers
 {
-    [SerializeField] private TextMeshProUGUI bombText;
-    [SerializeField] private TextMeshProUGUI timerText;
-    [SerializeField] private string baseTimerText;
-    [SerializeField] private string baseBombText;
-    [SerializeField] private GameObject _winUI;
-    [SerializeField] private GameObject _loseUI;
-
-    private float _timer;
-
-    private void Awake()
+    public class UIManager : MonoBehaviour
     {
-        _timer = 0;
-        timerText.text = baseTimerText + "00";
-        bombText.text = baseBombText;
-    }
+        [SerializeField] private TextMeshProUGUI bombText;
+        [SerializeField] private TextMeshProUGUI timerText;
+        [SerializeField] private string baseTimerText;
+        [SerializeField] private string baseBombText;
+        [SerializeField] private GameObject _winUI;
+        [SerializeField] private GameObject _loseUI;
 
-    private void Update()
-    {
-        _timer += Time.deltaTime;
+        private float _timer;
 
-        if (!(_timer >= 1)) return;
+        private void Awake()
+        {
+            _timer = 0;
+            timerText.text = baseTimerText + "00";
+            bombText.text = baseBombText;
+        }
+
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+
+            if (!(_timer >= 1)) return;
         
-        timerText.text = baseTimerText + Time.timeSinceLevelLoad.ToString("00");
-        _timer = 0;
-    }
+            timerText.text = baseTimerText + Time.timeSinceLevelLoad.ToString("00");
+            _timer = 0;
+        }
 
-    public void UpdateBombText(int bombCounter)
-    {
-        bombText.text = baseBombText + bombCounter.ToString("00");
-    }
+        public void UpdateBombText(int bombCounter)
+        {
+            bombText.text = baseBombText + bombCounter.ToString("00");
+        }
 
-    public void ShowWinUI()
-    {
-        _winUI.SetActive(true);
-    }
+        public void ShowWinUI()
+        {
+            _winUI.SetActive(true);
+        }
 
-    public void ShowLoseUI()
-    {
-        _loseUI.SetActive(true);
-    }
+        public void ShowLoseUI()
+        {
+            _loseUI.SetActive(true);
+        }
 
-    public void LoadGameScene()
-    {
-        SceneLoader.LoadGameScene();
-    }
+        public void LoadGameScene()
+        {
+            SceneLoader.LoadGameScene();
+        }
     
-    public void LoadLobbyScene()
-    {
-        SceneLoader.LoadLobbyScene();
+        public void LoadLobbyScene()
+        {
+            SceneLoader.LoadLobbyScene();
+        }
     }
 }
