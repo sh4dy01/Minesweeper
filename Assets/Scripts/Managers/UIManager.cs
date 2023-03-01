80c8e6327f538a5ac49170514671d2329e5fa46f
Arthur <45017124+Falaron@users.noreply.github.com>
2023-03-01 11:43:35 +0100

    basis props spawn

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -9,11 +9,21 @@ namespace Managers
         [SerializeField] private GameObject _loseUI;
         [SerializeField] private ParticleSystem _particle;
 
+        [Header("Props")]
+        [SerializeField] private GameObject countDown;
+        [SerializeField] private GameObject bombCounter;
+
         private float _timer;
+        private int _spawnPropsWidth;
+        private int _spawnPropsHeight;
 
         private void Awake()
         {
             _timer = 0;
+            _spawnPropsWidth = 9;
+            _spawnPropsHeight = 5;
+            RandomPropsPosition(countDown);
+            RandomPropsPosition(bombCounter);
         }
 
         private void Update()
@@ -23,6 +33,11 @@ namespace Managers
             if (!(_timer >= 1)) return;
 
             _timer = 0;
+        }
+
+        private void RandomPropsPosition(GameObject gameObject)
+        {
+            gameObject.transform.position = new Vector3(Random.Range(-_spawnPropsWidth, _spawnPropsWidth), Random.Range(-_spawnPropsHeight, _spawnPropsHeight), gameObject.transform.position.z);
         }
 
         public void UpdateBombText(int bombCounter)
