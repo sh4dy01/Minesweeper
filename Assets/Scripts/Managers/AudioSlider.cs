66ccad74fedf61c3445345ca141d9bb93ab4c571
Hugo.M <hmaestracci@gaming.tech>
2023-03-02 09:59:15 +0100

    new audio control manager

---
+++ Assets/Scripts/Managers/AudioSlider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace Managers
+{
+    public class AudioSlider : MonoBehaviour
+    {
+        [SerializeField] private AudioMixer _mixer;
+        [SerializeField] private Slider _slider;
+        [SerializeField] private AudioMixParameter _volumeParameter;
+        
+        private readonly Dictionary<AudioMixParameter, string> _volumeParameters = new()
+        {
+            //{AudioMixParameter.MasterVolume, "MasterVolume"},
+            {AudioMixParameter.MusicVolume, "MusicVolume"},
+            {AudioMixParameter.SoundVolume, "SoundVolume"}
+        };
+        
+        private string _currentVolumeParameterName;
+
+        private void Awake()
+        {
+            _currentVolumeParameterName = _volumeParameters[_volumeParameter];
+            _slider.onValueChanged.AddListener(OnChangeSlider);
+        }
+
+        private void Start()
+        {
+            _slider.value = PlayerPrefs.GetFloat(_currentVolumeParameterName, 0.01f);
+        }
+
+        private void OnChangeSlider(float value)
+        {
+            _mixer.SetFloat(_currentVolumeParameterName, Mathf.Log10(value) * 20);
+        }
+
+        private void OnDisable()
+        {
+            PlayerPrefs.SetFloat(_currentVolumeParameterName, _slider.value);
+        }
+
+        private enum AudioMixParameter
+        {
+            //MasterVolume //NOT USED
+            MusicVolume,
+            SoundVolume
+        }
+    }
+}
